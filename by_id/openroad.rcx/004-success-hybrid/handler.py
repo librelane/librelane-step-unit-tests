3c39862ac8c91b32030be3d92413105aa8a58474
Mohamed Gaber <donn@efabless.com>
2023-11-16 09:38:05 +0200

    Fix formatting

--- by_id/openroad.rcx/004-success-hybrid/handler.py
+++ by_id/openroad.rcx/004-success-hybrid/handler.py
@@ -2,22 +2,23 @@ import subprocess
 
 
 def handle(step):
-
     print(step.state_out["spef"])
     for corner in ["nom", "min", "max"]:
         spef_file = step.state_out["spef"][f"{corner}_*"]
         with open("run_sta.tcl", "w") as f:
             lib = step.config["LIB"]["*_tt_025C_1v80"][0]
-            f.write(f"""
-                    read_liberty {lib}
-                    read_verilog {step.state_out['nl']}
-                    read_verilog ./spm.nl.v
-                    link_design manual_macro_placement_test
-                    read_spef {spef_file}
-                    read_spef -path spm_inst_0 ./spm.{corner}.spef
-                    read_spef -path spm_inst_1 ./spm.{corner}.spef
-                    report_parasitic_annotation -report_unannotated > out.log
-                    """)
+            f.write(
+                f"""
+                read_liberty {lib}
+                read_verilog {step.state_out['nl']}
+                read_verilog ./spm.nl.v
+                link_design manual_macro_placement_test
+                read_spef {spef_file}
+                read_spef -path spm_inst_0 ./spm.{corner}.spef
+                read_spef -path spm_inst_1 ./spm.{corner}.spef
+                report_parasitic_annotation -report_unannotated > out.log
+                """
+            )
 
         result = subprocess.run(["sta", "-exit", "run_sta.tcl"], capture_output=True)
 
