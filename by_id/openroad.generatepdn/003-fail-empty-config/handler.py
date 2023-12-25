61ed5ed1ee2cc0815a7bedc5389c4bb5745109eb
Mohamed Gaber <donn@efabless.com>
2023-12-25 13:36:37 +0200

    Update PDN tests (#16)
    * Renamed `fail-epic` to `fail-empty-config`
    * Added special handler for `fail-misconnect`

--- by_id/openroad.generatepdn/003-fail-empty-config/handler.py
+++ by_id/openroad.generatepdn/003-fail-empty-config/handler.py
@@ -2,7 +2,7 @@ def handle(step, exception, caplog):
     assert exception is None
     assert (
         step.state_out.metrics["design__power_grid_violation__count__net:VPWR"] > 0
-    ), "Misconfigured PDN has no VPWR violations"
+    ), "Completely missing PDN did not report any VPWR nodes as disconnected"
     assert (
         step.state_out.metrics["design__power_grid_violation__count__net:VGND"] > 0
-    ), "Misconfigured PDN has no VGND violations"
+    ), "Completely missing PDN did not report any VGND nodes disconnected"
--- by_id/openroad.generatepdn/003-fail-epic/handler.py
+++
@@ -1,4 +0,0 @@
-def handle(step, exception, caplog):
-    assert exception is not None, "Check nodes didn't catch missing PDN"
-    error_msg = "Check connectivity failed"
-    assert error_msg in caplog.text, f"{error_msg} not found in log"
