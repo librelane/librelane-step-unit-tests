920c54b1c7994cc1bfc05670c590fbda9f359b99
Mohamed Gaber <donn@efabless.com>
2024-09-16 16:05:19 +0300

    Odb.SetPowerConnections not crashing with a hierarchical json netlist (#36)

--- by_id/odb.setpowerconnections/003-fail_mismatched_view/base_user_project_wrapper.sdc
+++
@@ -1,1 +0,0 @@
-../001-success/base_user_project_wrapper.sdc
--- by_id/odb.setpowerconnections/003-fail_mismatched_view/handler.py
+++
@@ -1,5 +0,0 @@
-def handle(step, caplog, exception):
-    assert exception is not None, "No exception raised for mismatched views"
-    assert (
-        "the LEF and Verilog views of the module may be mismatched" in caplog.text
-    ), "No diagnostic message printed for mismatched views"
