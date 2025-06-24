d80f1f641ebd2dcc482bda6d7a852a9475c1811b
Mohamed Gaber <me@donn.website>
2025-06-24 20:17:35 +0300

    fix lint

--- by_id/yosys.synthesis/001-success/handler.py
+++ by_id/yosys.synthesis/001-success/handler.py
@@ -5,6 +5,10 @@ import subprocess
 def handle(step, test):
     if "slang" in test:
         result = subprocess.call(
-            ["grep", "SLANG frontend", os.path.join(step.step_dir, "yosys-synthesis.log")],
+            [
+                "grep",
+                "SLANG frontend",
+                os.path.join(step.step_dir, "yosys-synthesis.log"),
+            ],
         )
         assert result == 0, "Slang was not used in the synthesis process"
