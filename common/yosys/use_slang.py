0a75094e2c5d2d844078b04fbfc66dbb4484f629
Mohamed Gaber <me@donn.website>
2025-07-02 12:20:49 +0300

    Updates for librelane#702 (#1)
    - openroad.dumprcvalues: update expected value calculation to reflect openroad bugfix
    - rename synlig tests to slang tests
    - update floorplan with new error

--- common/yosys/use_slang.py
+++ common/yosys/use_slang.py
@@ -1,6 +1,6 @@
 def process_input(state_in, config, step_cls, pdk_root, test):
     config_parsed = step_cls._load_config_from_file(config, pdk_root)
-    if "synlig" in test:
-        config_parsed = config_parsed.copy(USE_SYNLIG=True)
+    if "slang" in test:
+        config_parsed = config_parsed.copy(USE_SLANG=True)
 
     return state_in, config_parsed
