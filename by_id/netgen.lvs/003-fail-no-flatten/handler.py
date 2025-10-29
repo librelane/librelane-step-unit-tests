2224b0d9a24b0e90fd2e2befb96abd138db1ca57
Leo Moser <leomoser99@gmail.com>
2025-10-29 16:11:56 +0100

    fix: due to recent netgen changes adjust the expected value from 4 to 2
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- by_id/netgen.lvs/003-fail-no-flatten/handler.py
+++ by_id/netgen.lvs/003-fail-no-flatten/handler.py
@@ -1,7 +1,7 @@
 def handle(step):
     metric_name = "design__lvs_error__count"
     metric_value = step.state_out.metrics[metric_name]
-    expected_value = 4
+    expected_value = 2
 
     assert (
         metric_value == expected_value
