c847d6bc399983e6934f4d15bd054cf458d5992a
Leo Moser <leomoser99@gmail.com>
2026-05-29 11:24:53 +0200

    fix: allow a higher error count (#8)
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- by_id/yosys.synthesis/004-tristate/handler.py
+++ by_id/yosys.synthesis/004-tristate/handler.py
@@ -4,8 +4,8 @@ import subprocess
 
 def handle(step, test):
     assert (
-        step.state_out.metrics["synthesis__check_error__count"] == 1
-    ), f"Expected 1 synthesis__check_error__count but found: {step.state_out.metrics['synthesis__check_error__count']}"
+        step.state_out.metrics["synthesis__check_error__count"] >= 1
+    ), f"Expected 1 or more synthesis__check_error__count but found: {step.state_out.metrics['synthesis__check_error__count']}"
     if "synlig" in test:
         result = subprocess.call(
             ["grep", "UHDM", os.path.join(step.step_dir, "yosys-synthesis.log")],
